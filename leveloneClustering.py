4d187f7bfac099b56d0b541ec4cb221ee860fc94
Ashish <ashish.iiith@gmail.com>
2014-03-29 11:54:49 -0400

    Level 1 Clustering

---
+++ leveloneClustering.py
@@ -0,0 +1,69 @@
+
+from py2neo import neo4j
+from py2neo import rel
+from py2neo import cypher
+import pickle
+import sys
+import re
+
+
+def intersection(a, b):
+ 
+    return list(set(a) & set(b))
+
+def union(a, b):
+ 
+    return list(set(a) | set(b))
+
+def compute_jaccardIndex(list1, list2):
+
+    return (float(float(len(intersection(list1, list2)))/float(len(union(list1, list2)))))
+
+def main():
+
+    graph_db = neo4j.GraphDatabaseService("http://localhost:7474/db/data/")
+    data_file = sys.argv[1]  
+    fd = open(data_file, "r")
+    graph = pickle.load(fd)
+    nodes =  graph.nodes()
+    edges = graph.edges()   
+
+    #print nodes
+    #print edges
+    dict = {}
+
+    clusters = []
+    for vertex in nodes:
+	dict[str(vertex)] = []
+        query = neo4j.CypherQuery(graph_db, "MATCH (a)-[r]-(b) WHERE b.id="+"\""+str(vertex)+"\""+" RETURN a").execute()
+        for r in query:
+	    #print str(r.a)
+	    m = re.findall("\"id\":(.*?)}", str(r.a))
+	    node_id = m[0].strip("\"")
+            dict[str(vertex)].append(node_id)
+
+	flag = 0
+	#check if this vertex can be merged with previous clusters
+	for cluster in clusters:
+	    union_list = []
+	    for node in cluster:
+	    	union_list = union(union_list, dict[str(node)])
+	    index = compute_jaccardIndex(union_list, dict[str(vertex)])
+	    if index >= 0.3:
+		flag = 1
+		cluster.append(vertex)
+		break
+	
+	if flag == 0:
+	    #if node has not found cluster to merge with, form a separate cluster
+	    clusters.append([vertex])
+
+    
+    print "Clusters"
+
+    for cluster in clusters:
+	print cluster
+ 
+
+if __name__ == "__main__":
+    main()
